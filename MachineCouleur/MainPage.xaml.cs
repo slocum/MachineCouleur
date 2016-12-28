a53c913e8e102164601c04bd8115c0e71df2161a
JF Enond <jfenond@hotmail.com>
2016-12-28 11:44:17 +0100

    Création du projet
    Réseau de neuronnes

--- MachineCouleur/MainPage.xaml.cs
+++ MachineCouleur/MainPage.xaml.cs
@@ -1,10 +1,19 @@
-﻿using System;
+﻿using MachineCouleur.Metier;
+using SloIALib.ANNs.PMLs;
+using SloIALib.ANNs.PMLs.Donnees;
+using SloIALib.ANNs.PMLs.SloReseauNeurones;
+using SloIALib.Functions;
+using SloOutilsLib.DossiersFichiers;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.ApplicationModel;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Storage;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -22,9 +31,119 @@ namespace MachineCouleur
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        #region Variables
+
+        private LstEntrees _LstEntrees;
+        private LstSorties _LstSorties;
+        private Couleur _Couleur;
+
+        private ReseauNeurones _RN;
+        private MachineAApprendre _MaA;
+        private double _TxApprentissage;
+
+        #endregion
+
+        #region Constructeur
+
         public MainPage()
         {
             this.InitializeComponent();
+        }
+
+        #endregion
+
+        #region Implémentation
+
+        private void Init()
+        {
+            CreerListeEntrees();
+            CreerListeSorties();
+
+            _Couleur = new Couleur();
+            this.DataContext = _Couleur;
+
+            CreerReseauNeurones();
+        }
+
+        private void CreerListeEntrees()
+        {
+            if (_LstEntrees == null)
+            {
+                _LstEntrees = new LstEntrees();
+
+                _LstEntrees.Ajouter(new Entree("Rouge"));
+                _LstEntrees.Ajouter(new Entree("Vert"));
+                _LstEntrees.Ajouter(new Entree("Bleu"));
+
+                lstEntrees.ItemsSource = _LstEntrees.Observable;
+
+            }
+        }
+
+        private void CreerListeSorties()
+        {
+            if (_LstSorties == null)
+            {
+                _LstSorties = new LstSorties();
+
+                _LstSorties.Ajouter(new Sortie("Inconnue", true));
+                _LstSorties.Ajouter(new Sortie("Noir"));
+                _LstSorties.Ajouter(new Sortie("Blanc"));
+                _LstSorties.Ajouter(new Sortie("Gris"));
+                _LstSorties.Ajouter(new Sortie("Rouge"));
+                _LstSorties.Ajouter(new Sortie("Vert"));
+
+                lstSorties.ItemsSource = _LstSorties.Observable;
+            }
+        }
+
+        private void CreerReseauNeurones()
+        {
+            _TxApprentissage = 0.7;
+
+            _RN = new ReseauNeurones(_LstEntrees.Count, new int[] { 4, _LstSorties.Count }, new SigmoidFunction());
+            _MaA = new MachineAApprendre(_RN, _TxApprentissage);
+        }
+
+        private void NouvelleCouleur()
+        {
+            _Couleur.aleatoire();
+
+            _LstEntrees.Modifier(Couleur.ROUGE, _Couleur.Rouge);
+            _LstEntrees.Modifier(Couleur.VERT, _Couleur.Vert);
+            _LstEntrees.Modifier(Couleur.BLEU, _Couleur.Bleu);
+
+        }
+
+        private void NouvelEchantillon()
+        {
+            NouvelleCouleur();
+
+            _LstSorties.AjouterLstDouble(_RN.CalculerSorties(_LstEntrees.Doubles));
+        }
+        #endregion
+
+        #region Gestionnaire d'événements
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            Init();
+
+            base.OnNavigatedTo(e);
+        }
+
+        private void Btndemarrer_Click(object sender, RoutedEventArgs e)
+        {
+            NouvelEchantillon();
+        }
+
+        #endregion
+
+        private void RadioButton_Click(object sender, RoutedEventArgs e)
+        {
+            Echantillon echantillon = new Echantillon()
+            _MaA.Apprendre()
+            NouvelEchantillon();
         }
     }
 }
---
+++ MachineCouleur/Metier/Couleur.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Media;
+
+namespace MachineCouleur.Metier
+{
+    public class Couleur : INotifyPropertyChanged
+    {
+        #region Constantes
+
+        public const int ROUGE = 0;
+        public const int VERT = 1;
+        public const int BLEU = 2;
+
+        private const byte VALEUR_MAX = 255;
+
+        #endregion
+
+        #region Variables
+
+        private Random _Rand = new Random(DateTime.Now.Millisecond);
+
+
+        #endregion
+
+        #region Accesseurs
+
+        private string _Nom;
+        public string Nom
+        {
+            get
+            {
+                return _Nom;
+            }
+        }
+
+        private byte _Rouge;
+        public byte Rouge {
+            get { return _Rouge; }
+            set { _Rouge = limiterValeur(value); }
+        }
+
+        private byte _Vert;
+        public byte Vert {
+            get { return _Vert; }
+            set { _Vert = limiterValeur(value); }
+        }
+
+        private byte _Bleu;
+        public byte Bleu {
+            get { return _Bleu; }
+            set { _Bleu = limiterValeur(value); }
+        }
+
+        private Color _RVB;
+        public Color RVB
+        {
+            get
+            {
+                return _RVB;
+            }
+        }
+
+        SolidColorBrush _BrushCouleur;
+        public SolidColorBrush BrushCouleur
+        {
+            get
+            {
+                return _BrushCouleur;
+            }
+        }
+
+        #endregion
+
+        #region Constructeur
+
+        public Couleur()
+        {
+            setProprietes(0, 0, 0);
+        }
+
+        #endregion
+
+        #region Interface
+
+        public void aleatoire()
+        {
+            setProprietes(NumberRand(), NumberRand(), NumberRand());
+        }
+
+        #endregion
+
+        #region Implémentation
+
+        private byte limiterValeur(byte pValue)
+        {
+            if (pValue < 0) return 0;
+            if (pValue > VALEUR_MAX) return VALEUR_MAX;
+
+            return pValue;
+        }
+
+        private byte NumberRand()
+        {
+            return (byte)_Rand.Next(VALEUR_MAX + 1) ;
+        }
+
+        private void setProprietes(byte pRouge, byte pVert, byte pBleu)
+        {
+            Rouge = pRouge;
+            Vert = pVert;
+            Bleu = pBleu;
+
+            _BrushCouleur = new SolidColorBrush(Color.FromArgb(255, Rouge, Vert, Bleu));
+            onPropertyChanged(new PropertyChangedEventArgs("BrushCouleur"));
+
+            _RVB = _BrushCouleur.Color; 
+            onPropertyChanged(new PropertyChangedEventArgs("RVB"));
+
+            _Nom = _RVB.ToString();
+            onPropertyChanged(new PropertyChangedEventArgs("Nom"));
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void onPropertyChanged(PropertyChangedEventArgs e)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, e);
+            }
+        }
+
+        #endregion
+    }
+}
